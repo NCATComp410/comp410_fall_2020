dece640207902e24dcd00d6abaef1e8bde9925c2
claesmk <claesmk@gmail.com>
2020-11-30 10:17:56 -0500

    show logging buffered with corrected spacing

--- asa_parser/src/asa_parse.py
+++ asa_parser/src/asa_parse.py
@@ -460,7 +460,15 @@ class AsaParser(ShowTech):
 
     def show_logging_buffered(self):
         """Parser for show show logging buffered"""
-        return json.dumps({'text': self.get_show_section('logging buffered')})
+        logs = {}
+        """regex to make sure logged information is in correct format"""
+        lineNum = 1
+        for line in self.get_show_section('logging buffered'):
+            regex = "\w+\s+\d+\s+\d{4}\s+\d+\:+\d+\:+\d+\s+KP-systest-admin\s+\:\s+%ASA-4-711004\:\s+Task ran for \d+\s+\w+\,+\s+Process = \w+\s*\w*\s*\w*\, PC = \w+\, Call stack = "
+            logs.update({lineNum: re.search(regex, line, flags=0).group(0)})
+            lineNum += 1
+
+        return json.dumps(logs)
 
     def show_kernel_process(self):
         """Parser for show kernel process"""
